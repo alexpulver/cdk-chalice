0b30c2d4e0718b36f858d34cc7beb70dc2133cec
Alex Pulver <apulver@amazon.com>
2020-03-29 10:38:49 +0300

    Updated tests to run in random dir under /tmp

--- tests/test_cdk_chalice.py
+++ tests/test_cdk_chalice.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+import tempfile
 import unittest
 
 from aws_cdk import core as cdk
@@ -10,50 +11,55 @@ from cdk_chalice import Chalice, PackageConfig
 
 class ChaliceTestCase(unittest.TestCase):
 
-    CDK_OUT_DIR = os.path.join(os.getcwd(), 'cdk.out')
-    CHALICE_OUT_DIR = os.path.join(os.getcwd(), 'chalice.out')
-    CHALICE_APP_DIR = os.path.join(os.path.dirname(__file__), 'chalice_app')
-    CHALICE_APP_DEPLOYMENTS_DIR = os.path.join(CHALICE_APP_DIR, '.chalice', 'deployments')
-    CHALICE_APP_CONFIG_FILE = os.path.join(CHALICE_APP_DIR, '.chalice', 'config.json')
-    CHALICE_APP_STAGE_CONFIG = {
-        'api_gateway_stage': 'v1'
-    }
-
     def setUp(self) -> None:
-        with open(ChaliceTestCase.CHALICE_APP_CONFIG_FILE, 'w') as config_file:
+        self.temp_dir = tempfile.mkdtemp(dir='/tmp')
+        os.chdir(self.temp_dir)
+
+        self.cdk_out_dir = os.path.join(self.temp_dir, 'cdk.out')
+        self.chalice_out_dir = os.path.join(self.temp_dir, 'chalice.out')
+        shutil.copytree(os.path.join(os.path.dirname(__file__), 'chalice_app'),
+                        os.path.join(self.temp_dir, 'chalice_app'))
+        self.chalice_app_dir = os.path.join(self.temp_dir, 'chalice_app')
+        self.chalice_app_deployments_dir = os.path.join(
+            self.chalice_app_dir, '.chalice', 'deployments')
+        self.chalice_app_config_file = os.path.join(
+            self.chalice_app_dir, '.chalice', 'config.json')
+        self.chalice_app_stage_config = {
+            'api_gateway_stage': 'v1'
+        }
+
+        with open(self.chalice_app_config_file, 'w') as config_file:
             chalice_app_config = {
                 'version': '2.0',
-                'app_name': 'test',
+                'app_name': 'chalice',
             }
             json.dump(chalice_app_config, config_file, indent=2)
 
     def tearDown(self) -> None:
-        shutil.rmtree(ChaliceTestCase.CDK_OUT_DIR)
-        shutil.rmtree(ChaliceTestCase.CHALICE_OUT_DIR)
-        shutil.rmtree(ChaliceTestCase.CHALICE_APP_DEPLOYMENTS_DIR)
-        os.remove(ChaliceTestCase.CHALICE_APP_CONFIG_FILE)
+        shutil.rmtree(self.temp_dir)
 
     def test_package_using_subprocess(self) -> None:
-        app = cdk.App(outdir=ChaliceTestCase.CDK_OUT_DIR)
-        stack = cdk.Stack(app, 'Stack')
-        chalice = Chalice(stack, 'TestPackageUsingSubprocess',
-                          source_dir=ChaliceTestCase.CHALICE_APP_DIR,
-                          stage_config=ChaliceTestCase.CHALICE_APP_STAGE_CONFIG)
+        app = cdk.App(outdir=self.cdk_out_dir)
+        stack = cdk.Stack(app, 'TestSubprocess')
+        chalice = Chalice(stack, 'WebApi',
+                          source_dir=self.chalice_app_dir,
+                          stage_config=self.chalice_app_stage_config)
         self._synth_and_assert(app, chalice)
 
     def test_package_using_docker(self) -> None:
-        app = cdk.App(outdir=ChaliceTestCase.CDK_OUT_DIR)
-        stack = cdk.Stack(app, 'Stack')
+        app = cdk.App(outdir=self.cdk_out_dir)
+        stack = cdk.Stack(app, 'TestDocker')
         package_config = PackageConfig(use_container=True)
-        chalice = Chalice(stack, 'TestPackageUsingSubprocess',
-                          source_dir=ChaliceTestCase.CHALICE_APP_DIR,
-                          stage_config=ChaliceTestCase.CHALICE_APP_STAGE_CONFIG,
+        chalice = Chalice(stack, 'WebApi',
+                          source_dir=self.chalice_app_dir,
+                          stage_config=self.chalice_app_stage_config,
                           package_config=package_config)
         self._synth_and_assert(app, chalice)
 
     def _synth_and_assert(self, app: cdk.App, chalice: Chalice) -> None:
         cloud_assembly = app.synth()
-        cloudformation_template = cloud_assembly.get_stack_by_name('Stack').template
+        stack_name = cdk.Stack.of(chalice).stack_name
+        cloudformation_template = cloud_assembly.get_stack_by_name(stack_name).template
         self.assertTrue(os.path.exists(chalice.sam_package_dir))
         self.assertIsNotNone(chalice.sam_template)
         self.assertNotEqual(
