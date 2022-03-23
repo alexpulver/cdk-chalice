4e560d580d65397384e81f9da168ada9fc73fd37
Alex Pulver <alex.pulver@gmail.com>
2022-03-23 09:32:31 +0200

    Remove flake8 and bump dependencies
    Removing flake8 due to https://github.com/PyCQA/flake8/issues/1522.
    I couldn't find anything major that mypy + black + pylint don't cover.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,6 @@ set -o verbose
 
 bandit --ini .bandit
 black --check --diff .  # There is no configuration file for Black
-flake8 --config .flake8
 isort --settings-path .isort.cfg --check --diff .
 mypy --config-file .mypy.ini cdk_chalice.py
 pylint --rcfile .pylintrc cdk_chalice.py
