b35ea4b1f8053ae5a4ad5cb8028fb100e2ba1363
Alex Pulver <alex.pulver@gmail.com>
2022-03-24 14:19:49 +0200

    Pin pip-tools and pip to use dot package name

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,5 +4,8 @@ set -o errexit
 set -o verbose
 
 # Install project dependencies
+# Pinning pip-tools to 6.4.0 and pip to 21.3.1 due to
+# https://github.com/jazzband/pip-tools/issues/1576
 pip install pip-tools==6.4.0
+pip install pip==21.3.1
 pip-sync requirements.txt requirements-dev.txt
